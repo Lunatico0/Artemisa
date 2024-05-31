2b05d22fbb5cdf96bb5003393141f6bd05e19783
Patricio Pittana <pittanapatricio@gmail.com>
2024-05-30 22:23:21 -0300

    Correccion error en navBar con el uso de activeClassName

--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import categorias from "../../data/category.json"
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import categorias from "../../data/category.json";
 
 const NavBar = () => {
   return (
     <nav className='nav'>
       <ul className='navMenu'>
         <li className='navItem'>
-          <NavLink to="/" activeClassName="active" className='navLink' >Inicio</NavLink>
+          <NavLink to="/" className={({ isActive }) => isActive ? "active navLink" : "navLink"}>Inicio</NavLink>
         </li>
         {
           categorias.map((categoria) => {
             return (
               <li className='navItem' key={categoria.id}>
-                <NavLink to={`/category/${categoria.id}`} activeClassName="active" className='navLink' >{categoria.nombre}</NavLink>
+                <NavLink to={`/category/${categoria.id}`} className={({ isActive }) => isActive ? "active navLink" : "navLink"}>{categoria.nombre}</NavLink>
               </li>
-            )
+            );
           })
         }
       </ul>
     </nav>
-  )
+  );
 }
 
-export default NavBar
+export default NavBar;
