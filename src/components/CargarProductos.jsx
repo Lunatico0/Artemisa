f56f3bed006dbab423b6800c34c731b9b98a8759
Patricio Pittana <pittanapatricio@gmail.com>
2024-06-20 23:59:37 -0300

    componente para agregar productos, y estilos generales

--- src/components/CargarProductos.jsx
+++ src/components/CargarProductos.jsx
@@ -1,22 +1,55 @@
-import React from 'react'
-// import productos from '../data/productosArtemisa.json'
-// import cat from '../data/category.json'
-// import { collection, addDoc } from 'firebase/firestore'
-// import { db } from '../firebase/config'
+import React, { useState } from 'react';
+import { collection, addDoc, getDocs, query, where } from 'firebase/firestore';
+import { db } from '../firebase/config';
 
 const CargarProductos = () => {
+  const [selectedOption, setSelectedOption] = useState('categoria');
 
-  // const productosRef = collection(db, "categoria")
+  const handleSet = async (destino, origen) => {
+    try {
+      const ref = collection(db, destino);
 
-  // cat.forEach(prod => {
-  //   addDoc(productosRef, prod)
-  // });
+      if (destino === 'productos') {
+        // Consulta para verificar si el producto ya existe
+        const querySnapshot = await getDocs(query(ref, where('id', '==', origen.id)));
+
+        if (querySnapshot.empty) {
+          await addDoc(ref, origen);
+          console.log('Producto agregado correctamente.');
+        }
+      } else if (destino === 'categoria') {
+        const catQuerySnapshot = await getDocs(query(ref, where('categoriaId', '==', origen.categoriaId)));
+        const subCatQuerySnapshot = await getDocs(query(ref, where('subcategorias.subcategoriaId', '==', origen.subcategorias.subcategoriaId)));
+
+        if (catQuerySnapshot.empty) {
+          await addDoc(ref, origen);
+          console.log('Categoria agregada correctamente.');
+        }
+        if (subCatQuerySnapshot.empty) {
+          await addDoc(ref, origen);
+          console.log('Subcategoria agregada correctamente.');
+        }
+        console.log('Verificación de categoría y subcategorías.');
+      }
+    } catch (error) {
+      console.error('Error al agregar el producto o la categoría:', error);
+    }
+  };
+
+  const handleChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
 
   return (
-    <div>
-      Cargando Productos
+    <div style={{ position: "relative", zIndex: "3", color: "white", paddingTop: "10rem" }}>
+      <label htmlFor="inputState" className="formLabel">Array a cargar</label>
+      <select id="inputState" className="formSelect" onChange={handleChange} value={selectedOption}>
+        <option value="categoria">categoria</option>
+        <option value="productos">productos</option>
+      </select>
+      <button onClick={() => handleSet(selectedOption, selectedOption)}>Comenzar</button>
     </div>
-  )
-}
+  );
+};
 
-export default CargarProductos
+export default CargarProductos;
--- src/components/Carrito.jsx
+++ src/components/Carrito.jsx
@@ -1,26 +1,32 @@
-import React, { useContext } from 'react'
-import { CartContext } from '../context/CartContext'
-import { Link } from 'react-router-dom'
+import React, { useContext } from 'react';
+import { CartContext } from '../context/CartContext';
+import { Link } from 'react-router-dom';
 
 const Carrito = () => {
-
-  const { carrito, calcularTotal, eliminarProducto, vaciarCarrito } = useContext(CartContext)
+  const { carrito, calcularTotal, eliminarProducto, vaciarCarrito, agregarCantidad, quitarCantidad } = useContext(CartContext);
 
   return (
-    <div className="carruselContainer">
-      <div className="backgroundBanner"></div >
-      <div className='carro'>
-        {
-          carrito.map((prod) =>
-            <h1 key={prod.id}>{prod.nombre}: ${prod.precio}</h1>
-          )
-        }
-        <h2>Total: ${calcularTotal()}</h2>
-        <button onClick={vaciarCarrito} >Eliminar todos los productos</button>
-        <Link to={"/finalizar-compra"}>Ir a comprar</Link>
+    <>
+      <div className="carruselContainer">
+        <div className="backgroundBanner"></div>
       </div>
-    </div>
-  )
-}
+      <div className="carro">
+        {carrito.map((prod) => (
+          <div key={prod.id} className="producto">
+            <p>Descripción: {prod.descripcion}</p>
+            <p>Precio: ${prod.precio}</p>
+            <p>Cantidad: {prod.cantidad}</p>
+            <button onClick={() => quitarCantidad(prod.id)}>Quitar</button>
+            <button onClick={() => agregarCantidad(prod.id)}>Agregar</button>
+            <button onClick={() => eliminarProducto(prod.id)}>Eliminar</button>
+          </div>
+        ))}
+        <h2>Total: ${calcularTotal()}</h2>
+        <button onClick={vaciarCarrito}>Eliminar todos los productos</button>
+        <Link to="/finalizar-compra">Ir a comprar</Link>
+      </div>
+    </>
+  );
+};
 
-export default Carrito
+export default Carrito;
